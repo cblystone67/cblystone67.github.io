6548b9a97014fe56304199f5972ca352741fcf99
cblystone67 <christopherblystone1@gmail.com>
2026-02-23 10:01:31 -0800

    Creating a python file

---
+++ testing.py
@@ -0,0 +1,11 @@
+apple = "red apple"
+banana = "yellow banana"
+grape = "purple grape"
+def print_fruits():
+    print(apple)
+    print(banana)
+    print(grape)
+if __name__ == "__main__":
+    print_fruits()
+    
+    
